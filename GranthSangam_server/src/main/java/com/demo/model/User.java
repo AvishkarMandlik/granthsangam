63f81c92cd3675669fd897dde3bca69b7e722e9b
AvishkarMandlik <mandlikavi121@gmail.com>
2025-05-31 14:01:05 +0530

    feat: integrate PostgreSQL and dotenv for environment variable management

--- GranthSangam_server/src/main/java/com/demo/model/User.java
+++ GranthSangam_server/src/main/java/com/demo/model/User.java
@@ -14,6 +14,7 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 //@NoArgsConstructor
 //@AllArgsConstructor
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name = "users")
 public class User {
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
